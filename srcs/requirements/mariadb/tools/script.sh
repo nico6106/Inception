3cfde91b5f3742e49f10d62940de06926b046780
nicolas <nicolas.lesage@hotmail.fr>
2023-06-19 11:48:00 +0200

    mandatory seems OK

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,152 +1,28 @@
 
-#cd /var/run/
-#mkdir mysqld
-#chmod -R 755 /var/run/mysqld
-#cd mysqld
-#touch mysqld.sock
-
-#service mysql start;
-
-#
-#echo "creating database named " $SQL_DATABASE
-#mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-
-#echo "creating user " $SQL_USER " with pwd=" $SQL_PASSWORD
-#mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-
-#echo "grand privilege on " $SQL_DATABASE
-#mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-
-#echo "alter user rootpwd " $SQL_ROOT_PASSWORD
-#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-
-#echo "flush privileges "
-#mysql -e "FLUSH PRIVILEGES;"
-
-#mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-
-#exec mysqld_safe
-
-
-#service mysql start 
-
-#echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE ;" > db1.sql
-#echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD' ;" >> db1.sql
-#echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' ;" >> db1.sql
-#echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
-#echo "FLUSH PRIVILEGES;" >> db1.sql
-
-#mysql < db1.sql
-
-#kill $(cat /var/run/mysqld/mysqld.pid)
-
-#mysqld
-
-
-
-
-
-
-
-#service mysql start 
-
-#echo "service demarre \n"
-
-#echo "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE} ;" > db1.sql
-#echo "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}' ;" >> db1.sql
-#echo "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' ;" >> db1.sql
-#echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_USER}' ;" >> db1.sql
-#echo "FLUSH PRIVILEGES;" >> db1.sql
-
-#mysql < db1.sql
-
-#kill $(cat /var/run/mysqld/mysqld.pid)
-
-#echo "seems ok after kill \n"
-#mysqld --bind-address=0.0.0.0
-
-#echo "mysqld"
-
-
-
-
-#mysqladmin -u root password $SQL_ROOT_PASSWORD
-
+#si la BDD existe deja, ne pas la recreer
 if [ -d "/var/lib/mysql/$SQL_DATABASE" ]
 then
     echo "database already created"
 else
+    #demarrage service mysql
     service mysql start
-    #mysqladmin -u root -p $SQL_ROOT_PASSWORD
 
-
+    #etapes : 
+    #   1.creation database
+    #   2.creation user
+    #   3.donne les droits a l'utilisateur de mariadb
+    #   4.donne un nouveau mdp pour root
     mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
-
     mysql -e "CREATE USER IF NOT EXISTS ${SQL_USER}@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-
-    #mysql -u root -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-
     mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO ${SQL_USER}@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
     mysql -e "FLUSH PRIVILEGES;"
     mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('$SQL_ROOT_PASSWORD');"
-    #echo "alter user rootpwd " $SQL_ROOT_PASSWORD
-    #mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-
     sleep 3
-    #echo "flush privileges "
-    #mysql -e "FLUSH PRIVILEGES;"
 
-
-
-    #mysql -e "USE '${SQL_DATABASE}'; GRANT ALL PRIVILEGES ON * TO '${SQL_USER}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-    echo "here2"
-    #mysql -e "alter user 'root'@'localhost' identified by '${SQL_ROOT_PASSWORD}'";
-    #mysql -e "FLUSH PRIVILEGES;"
-
-
-    #mysql -e "CREATE DATABASE \`${SQL_DATABASE}\`;"
-    #mysql -e "CREATE USER \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-    #mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '\`${SQL_USER}\`'@'%';"
-    ##mysql -e "FLUSH PRIVILEGES;"
-
-    #mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-
+    #root arrete mariadb apres configuration
     mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 fi
 
+#demarage de mysqld
 exec mysqld_safe
 
-#kill $(cat /var/run/mysqld/mysqld.pid)
-#mysqld --bind-address=0.0.0.0
-
-
-#mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-
-#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-
-
-
-
-
-
-
-#service mysql start 
-
-#echo "service demarre \n"
-
-#mysql -e "CREATE DATABASE IF NOT EXISTS databse ;"
-#mysql -e  "CREATE USER IF NOT EXISTS 'michel'@'%' IDENTIFIED BY 'michel' ;"
-#mysql -e  "GRANT ALL PRIVILEGES ON databse.* TO 'michel'@'%' ;"
-#mysql -e  "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;"
-#mysql -e  "FLUSH PRIVILEGES;"
-
-#mysql < db1.sql
-
-#echo "commandes ok"
-
-#kill $(cat /var/run/mysqld/mysqld.pid)
-
-#echo "seems ok after kill"
-#mysqld --bind-address=0.0.0.0
-
-#echo "mysqld cmd"
--- srcs/requirements/wordpress/tools/config_wpcli.sh
+++ srcs/requirements/wordpress/tools/config_wpcli.sh
@@ -2,115 +2,28 @@
 
 sleep 8
 
-#dossier
 DATADIR='/var/www/html/'
 
-
-# if [ -f ./wp-config.php ]
-# then
-	# echo "wordpress already downloaded, deleting it"
-	# cd /var/www/
-	# rm -rf *
-	# ls -la
-	# mkdir -p wordpress
-	# cd /var/www/html
-# fi
-
+#si WP deja installe, on ne le configure pas a nouveau 
 if [ -f ./wp-config.php ]
 then
 	echo "wordpress already downloaded"
 else
-	# cd /var/www/html
-	#wget http://wordpress.org/latest.tar.gz
-	#tar xfz latest.tar.gz
-	#mv wordpress/* .
-	#rm -rf latest.tar.gz
-	#rm -rf wordpress
-
-	#mv wp-config-sample.php wp-config.php
-
+	#installation wp et configuration
 	wp --allow-root core download
 	wp --allow-root config create --dbname=${SQL_DATABASE} --dbuser=${SQL_USER} --dbpass=${SQL_PASSWORD} --dbhost="mariadb":"3306" 
-	#--dbprefix='wp_'
-	echo "ok for config"
 	wp --allow-root --skip-email core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USR} --admin_password=${WP_ADMIN_PWD} --admin_email=${WP_ADMIN_EMAIL}
-	echo "ok for core install"
+	#ajout utilisateur WP
 	wp --allow-root user create $WP_USR $WP_EMAIL --role='editor' --user_pass=${WP_PWD}
-	echo "seems ok?"
 
-	# #Inport env variables in the config file
-	# sed -i "s/username_here/$SQL_USER/g" wp-config-sample.php
-	# sed -i "s/password_here/$SQL_PASSWORD/g" wp-config-sample.php
-	# sed -i "s/localhost/$SQL_HOSTNAME/g" wp-config-sample.php
-	# sed -i "s/database_name_here/$SQL_DATABASE/g" wp-config-sample.php
-	# cp wp-config-sample.php wp-config.php
+	wp theme install twentytwentyone --allow-root
+	wp theme activate twentytwentyone --allow-root
+
 fi
 
+#modification des droits apres installation (pour etre sur...)
 cd /var/www/html
 chown -R www-data:www-data /var/www/html
 
-ls -la
-#chmod 777 *
-
-
-#bash
+#lancement php7.3
 /usr/sbin/php-fpm7.3 -F
-
-#if [1]
-#then
-#	echo oups
-#else
-
-	#echo "try to create config wordpress"
-
-	#cd /var/www/html
-
-	##installation WP-CLI : telechargement archive + installation (changement en executable + deplacer dans dossier)
-	##wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-	##chmod +x wp-cli.phar
-	##mv wp-cli.phar /usr/local/bin/wp
-
-	#mv /var/www/wp-config.php /var/www/html/wp-config.php
-	#sed -i -r "s/SQL_DATABASE/$SQL_DATABASE/1"   wp-config.php
-	#sed -i -r "s/SQL_USER/$SQL_USER/1"  wp-config.php
-	#sed -i -r "s/SQL_PASSWORD/$SQL_PASSWORD/1"    wp-config.php
-
-
-	#cd /var/www/html/
-
-	#wp core download --allow-root
-
-	##wp core config --allow-root --dbname='wordpress' --dbuser='wpuser' --dbpass='password' --dbhost='mariadb:3306' --dbprefix='wp_'
-
-	##wp core install --allow-root --url='http://nlesage.42.fr' --title='Blog Title' --admin_user='adminuser' --admin_password='password' --admin_email='email@domain.com' --skip-email
-
-	#wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-
-	#wp plugin install wordpress-seo --allow-root
-	#wp plugin activate wordpress-seo --allow-root
-
-	#wp theme install twentyfourteen --allow-root
-	#wp theme activate twentyfourteen --allow-root
-
-
-	##wp config create	--allow-root \
-	##					--dbname=$SQL_DATABASE \
-	##					--dbuser=$SQL_USER \
-	##					--dbpass=$SQL_PASSWORD \
-	##					--dbhost=mariadb:3306 --path='/var/www/html'
-
-	##--url=$DOMAIN_NAME/
-
-	##wp core install --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-
-	##wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-
-	##wp theme install astra --activate --allow-root
-
-
-	#sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-
-	##/usr/sbin/php7.3 -F
-	##/usr/sbin/php-fpm7.3 -F
-
-#fi
